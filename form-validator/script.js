aab8c99f20e0fe2b18e9e7117352ba68232b0bb8
JordBant <jaybant23@gmail.com>
2021-11-11 09:26:19 -0500

    feat: Written javascript for form/comparing passwords

---
+++ form-validator/script.js
@@ -0,0 +1,25 @@
+const allInput = document.getElementsByTagName('input');
+const btn = document.getElementsById('submit');
+const password = document.getElementsById('password');
+const confirm_password = document.getElementId('confirm');
+const email = document.getElementId('email');
+const number = document.getElementId('number');
+const username = document.getElementId('username');
+
+btn.addEventListener('submit', e => e.preventDefault)
+
+const form_val = () => 
+{
+
+}
+
+function invalidInput()
+{
+    let og_password = password.value;
+    let to_compare = confirm_password.value;
+
+    function check(original, confirmer)
+    {
+        const  = document.getElementId('confirm');
+    }
+}
