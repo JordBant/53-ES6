bd98ec74179d16b1151d425e6eb40dbc0123e1da
JordBant <jaybant23@gmail.com>
2021-12-08 01:32:27 -0500

    Nearly complete Game
    feat:
    - Game logic via JS is nearly complete
    - Reset Button has yet to be enabled
    - Styling is nearly complete
       {Guess items must be coded to show the range in which the random number is located via color-temperature(red for hot, blue for cold, etc)}

--- 1_100/script.js
+++ 1_100/script.js
@@ -1,23 +1,145 @@
 'use strict';
 
+const body = document.getElementsByTagName('body')
 const input = document.getElementById("guess");
 const submit = document.getElementById("submit");
 const list = document.getElementById("guesslist");
-const rand = Math.floor(Math.random()*100);
+const inputArea = document.getElementById("input-area")
 
-submit.addEventListener('click', game());
+const rand = Math.floor(Math.random()*10);
+let count = 0;
+
+submit.addEventListener('click', game);
 
 function game()
 {
-    const userInput = input.value;
-    let count = 0;
+    const userInput = Number(input.value);
+    const newLi = document.createElement('li');
+        newLi.setAttribute("id", "guessItem");
 
-    if(userInput !== rand)
-    {
-        let guessItem = document.createElement('li').setAttribute('class', 'li-div')
-            guessItem.innerText(userInput.toString());
-        list.append(guessItem);
-        let userInput = '';
-        count++;
+    const reset = document.createElement('button');
+        reset.setAttribute("id", "reset");
+        reset.append('Reset Game');
+
+    switch (userInput) {
+
+        // Rand - 5 ≤ userInput ≤ Rand + 5
+        case rand + 5 > userInput:
+        case userInput > rand:
+
+        case rand - 5 < userInput:
+        case userInput < rand:
+
+        case rand !== userInput:
+        case count !== 10:
+
+        // Apply redish-orange color styling of list item
+            newLi.append(userInput);
+            list.append(newLi);
+
+            count += 1;
+            break;
+    
+        case rand + 12 > userInput:
+        case userInput > rand:
+    
+        case rand - 12 < userInput:
+        case userInput < rand:
+
+        case rand !== userInput:
+        case count !== 10:
+        // Apply orangish color styling of list item
+            newLi.append(userInput);
+            list.append(newLi);
+            
+            count += 1;
+            break;
+
+        case rand + 20 > userInput:
+        case userInput > rand:
+    
+        case rand - 20 < userInput:
+        case userInput < rand:
+
+        case rand !== userInput:
+        case count !== 10:
+
+        // Apply yellow color styling of list item
+            newLi.append(userInput);
+            list.append(newLi);
+            
+            count += 1;
+            break;
+
+        case rand + 29 > userInput:
+        case userInput > rand:
+
+        case rand - 29 < userInput:
+        case userInput < rand:
+
+        case rand !== userInput:
+        case count !== 10:
+        // Apply yellow color styling of list item
+            newLi.append(userInput);
+            list.append(newLi);
+            
+        count += 1;
+        break;
+
+        case rand + 34 > userInput:
+        case userInput > rand:
+
+        case rand - 34 < userInput:
+        case userInput < rand:
+
+        case rand !== userInput:
+        case count !== 10:
+        // Apply yellow-green color styling of list item
+            newLi.append(userInput);
+            list.append(newLi);
+                
+        count += 1;
+        break;
+
+        case rand + 40 > userInput:
+        case userInput > rand:
+
+        case rand - 40 < userInput:
+        case userInput < rand:
+
+        case rand !== userInput:
+        case count !== 10:
+        // Apply minty greeny color styling of list item
+            newLi.append(userInput);
+            list.append(newLi);
+            
+            count += 1;
+            break;
+
+        case rand + 40 < userInput:
+        case rand - 40 > userInput:
+
+        case rand !== userInput:
+        case count !== 10:
+        // Apply minty greeny color styling of list item
+            newLi.append(userInput);
+            list.append(newLi);
+            
+            count += 1;
+            break;
+
+        default:
+            newLi.append('nice');
+            list.append(newLi);
+    
+            inputArea.replaceChild(reset, submit);
+            input.remove();
+            break;
     }
+    
+    // reset.addEventListener('click', gameReset);
 }
+
+// const gameReset = () =>{
+
+// }
