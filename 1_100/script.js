cab91935c86b83255294f1b1a2ec36c73cce5a8a
JordBant <jaybant23@gmail.com>
2021-12-11 20:53:04 -0500

    feat: hot-cold mechanic
    Temperature color works as numbers are in a set range

--- 1_100/script.js
+++ 1_100/script.js
@@ -6,7 +6,7 @@ const submit = document.getElementById("submit");
 const list = document.getElementById("guesslist");
 const inputArea = document.getElementById("input-area")
 
-const rand = 50 /*Math.floor(Math.random()*100)*/;
+const rand =  Math.floor(Math.random()*100);
 let count = 1;
 
 submit.addEventListener('click', game);
@@ -23,75 +23,90 @@ function game()
 
     switch (true)
     {        
+        case(userInput !== rand && count === 10):
+            // You ran out of turns
+        break;
+
+        case (userInput <= 0 || userInput >= 101):
+            //Submit button will say Enter Number to start game
+            submit.innerText = 'Enter In-Bound Number to start Game.'
+            input.value = "";
+            break;
+        
         case (rand - 5 < userInput && userInput < rand + 5 && rand !== userInput && count !== 10):
-            newLi.style.border = "7px solid red";
+            newLi.style.border = "7px solid #F70000";
             newLi.append(userInput);
             list.append(newLi);
 
+            input.value = "";
             count += 1;
             break;
     
         case (rand - 10 < userInput && userInput < rand + 10 && rand !== userInput && count !== 10):
-            newLi.style.border = "7px solid orange";
+            newLi.style.border = "7px solid #EE5E1B";
             newLi.append(userInput);
             list.append(newLi);
             
+            input.value = "";
             count += 1;
             break;
 
         case (rand - 20 < userInput && userInput < rand + 20 && rand !== userInput && count !== 10):
-            newLi.style.border = "7px solid yellow";
+            newLi.style.border = "7px solid #F9A213";
             newLi.append(userInput);
             list.append(newLi);
             
+            input.value = "";
             count += 1;
             break;
 
 
         case (rand - 30 < userInput && userInput < rand + 30 && rand !== userInput && count !== 10):
-            newLi.style.border = "7px solid green";
+            newLi.style.border = "7px solid #ABEBC6";
             newLi.append(userInput);
             list.append(newLi);
             
+            input.value = "";
             count += 1;
             break;
 
         case (rand - 35 < userInput && userInput < rand + 35 && rand !== userInput && count !== 10):
-            newLi.style.border = "7px solid purple";
+            newLi.style.border = "7px solid #76D7C4";
             newLi.append(userInput);
             list.append(newLi);
                 
+            input.value = "";
             count += 1;
             break;
 
         case (rand - 40 <= userInput && userInput <= rand + 40 && rand !== userInput && count !== 10):
-            newLi.style.border = "7px solid blue";
+            newLi.style.border = "7px solid #C1FCC8 ";
             newLi.append(userInput);
             list.append(newLi);
             
+            input.value = "";
             count += 1;
             break;
 
         case (userInput < rand - 40 || rand + 40 < userInput && rand !== userInput && count !== 10):
-            newLi.style.border = "7px solid grey";
+            newLi.style.border = "7px solid #C1FCE4";
             newLi.append(userInput);
             list.append(newLi);
             
+            input.value = "";
             count += 1;
             break;
 
         default:
-            newLi.append('nice');
-            list.append(newLi);
-    
+            input.value = "";
             inputArea.replaceChild(reset, submit);
+            reset.addEventListener('click', gameReset);
+
             input.remove();
             break;
     }
-    
-    // reset.addEventListener('click', gameReset);
 }
 
-// const gameReset = () =>{
-
-// }
+const gameReset = () =>{
+    
+}
