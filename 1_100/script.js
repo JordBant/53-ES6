ed84ccf347ade668a34f6ad6d8cca8c303e4cb8a
JordBant <jaybant23@gmail.com>
2021-12-16 18:44:47 -0500

    added html structure to page

--- 1_100/script.js
+++ 1_100/script.js
@@ -41,7 +41,7 @@ function game()
             break;
         
         case (rand - 5 < userInput && userInput < rand + 5 && rand !== userInput && list.childElementCount < 10):
-            newLi.style.border = "7px solid #F70000";
+            newLi.style.backgroundColor = "#F70000";
             newLi.append(userInput);
             list.append(newLi);
 
@@ -49,7 +49,7 @@ function game()
             break;
     
         case (rand - 10 < userInput && userInput < rand + 10 && rand !== userInput && list.childElementCount < 10):
-            newLi.style.border = "7px solid #EE5E1B";
+            newLi.style.backgroundColor = "#EE5E1B";
             newLi.append(userInput);
             list.append(newLi);
             
@@ -57,7 +57,7 @@ function game()
             break;
 
         case (rand - 20 < userInput && userInput < rand + 20 && rand !== userInput && list.childElementCount < 10):
-            newLi.style.border = "7px solid yellow";
+            newLi.style.backgroundColor = "yellow";
             newLi.append(userInput);
             list.append(newLi);
             
@@ -65,7 +65,7 @@ function game()
             break;
 
         case (rand - 30 < userInput && userInput < rand + 30 && rand !== userInput && list.childElementCount < 10):
-            newLi.style.border = "7px solid #bbe25f";
+            newLi.style.backgroundColor = "#bbe25f";
             newLi.append(userInput);
             list.append(newLi);
             
@@ -73,7 +73,7 @@ function game()
             break;
 
         case (rand - 35 < userInput && userInput < rand + 35 && rand !== userInput && list.childElementCount < 10):
-            newLi.style.border = "7px solid #76D7C4";
+            newLi.style.backgroundColor = "#76D7C4";
             newLi.append(userInput);
             list.append(newLi);
                 
@@ -81,7 +81,7 @@ function game()
             break;
 
         case (rand - 40 <= userInput && userInput <= rand + 40 && rand !== userInput && list.childElementCount < 10):
-            newLi.style.border = "7px solid #C1FCC8 ";
+            newLi.style.backgroundColor = "#C1FCC8 ";
             newLi.append(userInput);
             list.append(newLi);
             
@@ -89,7 +89,7 @@ function game()
             break;
 
         case (userInput < rand - 40 || rand + 40 < userInput && rand !== userInput && list.childElementCount < 10):
-            newLi.style.border = "7px solid #C1FCE4";
+            newLi.style.backgroundColor = "#C1FCE4";
             newLi.append(userInput);
             list.append(newLi);
             
