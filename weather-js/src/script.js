18e750f7f76c050600b8ad87532baf69dfbf3bbf
JordBant <jaybant23@gmail.com>
2022-02-09 16:37:13 -0500

    Added Icons and styles
    Added Icon and updated styling of the weather cards and fonts

--- weather-js/src/script.js
+++ weather-js/src/script.js
@@ -4,12 +4,12 @@
 // const weatherCard3;
 // const weatherCard4;
 
-//const degree = document.getElementById('temperature');
+// const degree = document.getElementById('temperature');
 
 // async function getWeather()
 // {
 //     const response = await fetch(
-//         `https://api.tomorrow.io/v4/timelines?location=-73.98529171943665,40.75872069597532&fields=${'weatherCode'}&fields=${'temperature'}&fields=${'precipitationType'}&fields=${'precipitationProbability'}&fields=${'visibility'}&fields=${'humidity'}&timesteps=current&units=imperial&apikey=r02b5dPj9KQ4f1zJXRjErMBgJtUmlQpL`);
+//         `https://api.tomorrow.io/v4/timelines?location=-73.98529171943665,40.75872069597532&fields=${'weatherCode'}&fields=${'temperature'}&fields=${'precipitationType'}&fields=${'precipitationProbability'}&fields=${'visibility'}&fields=${'humidity'}&timesteps=1h&units=imperial&apikey=r02b5dPj9KQ4f1zJXRjErMBgJtUmlQpL`);
 //     const data = await response.json();
 //     const dataObj = data.data.timelines[0].intervals[0].values;
 
