fb0999419f29f3dbb27109f16093876a3f144599
JordBant <jaybant23@gmail.com>
2022-03-23 11:30:27 -0400

    i wont be doing anything with route right now

--- weather-js/src/server/routes.js
+++ weather-js/src/server/routes.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const router = express.Router();
+// const express = require('express')
+// const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json 
-})
+// router.get('/', (req, res) => {
+//     res.json 
+// })
